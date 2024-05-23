95882773a0b5ec304273c50d97b1106904a90a31
Cristian Torrado <cristiantorrado@torradodesarrollos.ar>
2024-05-22 21:11:33 -0300

    td_bookcamp_widgets_notes_app_03

--- widgets-example-1/td_bootcamp_widget_notes_app/lib/home.dart
+++ widgets-example-1/td_bootcamp_widget_notes_app/lib/home.dart
@@ -1,22 +1,83 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<String> _notesList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _notesList = [];
+  }
+
+  void _onFabTap(){
+    setState(() {
+       var newNote = generateRandomString();
+       _notesList.add(newNote);
+        print(newNote);
+        print(_notesList.length);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Notes', style: Theme.of(context).textTheme.displaySmall),
-          actions: const [
-            FloatingActionButton.small(onPressed: null, child: Icon(Icons.search)),
-            Padding(
-              padding: EdgeInsets.fromLTRB(21,0,25,0),
-              child: FloatingActionButton.small(onPressed: null, child: Icon(Icons.info)),
+          actions: [
+            Theme(
+              data: _getFabCustomTheme(context: context),
+              child: const FloatingActionButton.small(onPressed: null, child: Icon(Icons.search))),
+             Padding(
+              padding: const EdgeInsets.fromLTRB(21,0,25,0),
+              child: Theme(
+                data: _getFabCustomTheme(context: context),
+                child: const FloatingActionButton.small(onPressed: null, child: Icon(Icons.info))),
             ),
           ],
         ),
-        body: SizedBox(
+        body: ListView(children: _notesList.map((text) => _cardFactory(text: text)).toList(),) ,
+        floatingActionButton: Theme(
+          data: _getFabCustomTheme(context: context), 
+          child: FloatingActionButton(
+            onPressed: _onFabTap, child: Icon(Icons.add)),
+        ),
+    );
+  }
+
+  ThemeData _getFabCustomTheme({required BuildContext context}){
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    return Theme.of(context)
+            .copyWith(colorScheme: Theme.of(context)
+              .colorScheme.copyWith(
+                primaryContainer: brightness == Brightness.light ? const Color(0xFFFFFFFF) : const Color(0xFF252525),
+                onPrimaryContainer: brightness == Brightness.dark ? const Color(0xFFFFFFFF) : const Color(0xFF252525)));
+  }
+
+  Widget _cardFactory({required String text}) {
+     return Padding(
+             padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+             child: Card( color: Colors.green, child:  Padding(
+               padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 45), 
+                child: Text(text, 
+                style: Theme.of(context).textTheme.headlineSmall!
+                .copyWith(color:Colors.black ))),
+             ),
+           );
+  }
+
+  /*
+   
+  SizedBox(
           width: double.infinity,
           height: double.infinity,
           child: Column(
@@ -29,15 +90,17 @@ class HomeScreen extends StatelessWidget {
               copyWith(fontWeight: FontWeight.w300),)
             ],
           ),
-        ),
-        floatingActionButton: Theme(
-          data: Theme.of(context)
-            .copyWith(floatingActionButtonTheme: Theme.of(context)
-            .floatingActionButtonTheme
-            .copyWith(backgroundColor: const Color(0xFF252525))), 
-          child: const FloatingActionButton(
-            onPressed: null, child: Icon(Icons.add)),
-        ),
-    );
-  }
+        ), 
+   
+   
+  */
+}
+
+String generateRandomString() {
+    const String chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    final Random random = Random();
+    return List.generate(Random()
+      .nextInt(50), 
+      (index) => chars[random.nextInt(chars.length)])
+      .join();
 }

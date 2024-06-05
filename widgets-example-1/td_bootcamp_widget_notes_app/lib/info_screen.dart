ad9d038d989dbe20f73996d670c5eec25d4c7d59
Cristian Torrado <cristiantorrado@torradodesarrollos.ar>
2024-06-05 20:57:22 -0300

    Clase 17 - Text Styles

--- widgets-example-1/td_bootcamp_widget_notes_app/lib/info_screen.dart
+++ widgets-example-1/td_bootcamp_widget_notes_app/lib/info_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:td_bootcamp_widget_notes_app/themes/util.dart';
 
 class InfoScreen extends StatelessWidget{
@@ -11,26 +10,49 @@ class InfoScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: Theme(
-              data: getFabCustomTheme(context: context),
-              child: FloatingActionButton.small(onPressed: (){
-                Navigator.of(context).pop();
-              }, 
-              child: const Icon(Icons.chevron_left_outlined))),
+          leadingWidth: 77,
+          leading: Padding(
+            padding: const EdgeInsets.fromLTRB(25.0,8.0,8.0,4.0),
+            child: Theme(
+                data: getFabCustomTheme(context: context),
+                child: FloatingActionButton.small(onPressed: (){
+                  Navigator.of(context).pop();
+                }, 
+                child: const Icon(Icons.chevron_left_outlined))),
+          ),
           actions: [
             Theme(
               data: getFabCustomTheme(context: context),
-              child: const FloatingActionButton.small(onPressed: null, child: Icon(Icons.edit))),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(0,0,25,0),
+                child: FloatingActionButton.small(onPressed: null, child: Icon(Icons.edit)),
+              )),
           ],
         ), 
-      body: const Column(children: [
-          Text("Book Review : The Design of Everyday Things by Don Norman"), 
-          Text('''The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.
-
-Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. 
-
-If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.''')
-        ])
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 27.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                Text("Book Review : The Design of Everyday Things by Don Norman", style: Theme.of(context).textTheme.displaySmall), 
+                const SizedBox(height: 38),
+                RichText(text:
+                  TextSpan(style: Theme.of(context).textTheme.headlineSmall, 
+                  children:  [
+                    const TextSpan(text: "The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two."),
+                    const TextSpan(text: "\n\n"),
+                    const TextSpan(text: "Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines."),
+                    const TextSpan(text: "\n\n"),
+                    TextSpan(text: "If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.", 
+                      style: Theme.of(context).textTheme.headlineSmall!
+                      .copyWith(fontStyle: FontStyle.italic))
+                  ]))
+              ]),
+          ),
+        ),
+      )
       ) ;
   }
 
